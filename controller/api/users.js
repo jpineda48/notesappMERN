b7ce2a738d3f86c55abd70ad6a4bd108141ce075
jpineda48 <jacquelinepineda48@gmail.com>
2023-10-01 07:50:22 -0400

    auth token complete

--- controller/api/users.js
+++ controller/api/users.js
@@ -1,13 +1,29 @@
+const User = require('../../models/user')
+const jwt = require('jsonwebtoken')
+
 module.exports = {
     create
   };
   
-  function create(req, res) {
-    // Baby step...
-    res.json({
-      user: {
-        name: req.body.name,
-        email: req.body.email
-      }
-    });
+  async function create(req, res) {
+  try{
+    const user = await User.create(req.body)
+    const token = createJWT(user)
+    res.json(token)
+  }catch{
+    res.status(400).json(err)
+
+  }
+  }
+
+
+  ///helper function
+
+  function createJWT(user){
+    return jwt.sign(
+      // data payload
+      { user },
+      process.env.SECRET,
+      { expiresIn: '24h' }
+    );
   }
